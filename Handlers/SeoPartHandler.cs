db50668f7a94b4c6dd67171074eecfc8279fcee5
Thierry Fleury <thierry@codinlab.fr>
2017-02-01 19:50:26 +0100

    Add indexing to SeoPart

---
+++ Handlers/SeoPartHandler.cs
@@ -0,0 +1,18 @@
+﻿using Codinlab.SSEO.Models;
+using Codinlab.SSEO.Services;
+using Orchard.ContentManagement.Handlers;
+
+namespace Codinlab.SSEO.Handlers
+{
+    public class SeoPartHandler : ContentHandler
+    {
+        public SeoPartHandler(ISeoService seoService)
+        {
+            OnIndexing<SeoPart>((context, seoPart) => context.DocumentIndex
+                .Add("seo-description", seoService.GetDescription(seoPart)).Analyze()
+                .Add("seo-keywords", seoService.GetKeywords(seoPart)).Analyze()
+                .Store()
+            );
+        }
+    }
+}
